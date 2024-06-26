13692a71764e3494c521ff3a97639a9a1d67b42a
Anggri <aanganggriawan95@gmail.com>
2024-06-26 10:10:37 +0700

    Push

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
 import Calendar from './pages/Calendar';
 import Chart from './pages/Chart';
-import ECommerce from './pages/Dashboard/ECommerce';
+import Pegawai from './pages/Dashboard/Pegawai';
 import FormElements from './pages/Form/FormElements';
 import FormLayout from './pages/Form/FormLayout';
 import Profile from './pages/Profile';
@@ -42,7 +42,7 @@ function App() {
           element={
             <>
               <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <ECommerce />
+              <Pegawai />
             </>
           }
         />
